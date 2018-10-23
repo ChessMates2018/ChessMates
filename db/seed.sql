370d6bd30c047dd8384fa1f86fdde7385d2deea6
Jordan Smithson <jordantroysmithson@gmail.com>
2018-10-23 12:20:47 -0600

    added schema and seed

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+insert into users (
+  username, password, rating, image, email, first_name, last_name, is_friend, is_online, wins, losses
+)
+values (
+'Knight', 'password', 90000, '../src/images/brady.jpg', 'bradyesnuggs@gmail.com', 'Brady', 'Snuggs', false, false, 0, 0
+), (
+'Bishop', 'password', 90000, '../src/images/michelle.jpg', 'michelle.editor2@gmail.com', 'Michelle', 'Jones', false, false, 0, 0
+), (
+'Rook', 'password', 90000, '../src/images/jordan.jpg', 'jordantroysmithson@gmail.com', 'Jordan', 'Smithson', false, false, 0, 0
+);
+
