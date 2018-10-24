623b216652a2c6647a44d22e2cc8b4b3102dc8c2
Brady Snuggs <dronebug360@gmail.com>
2018-10-24 12:24:04 -1000

    state, map, and rendering are set in RecentGames.js.

--- src/profile/components/RecentGames/RecentGames.js
+++ src/profile/components/RecentGames/RecentGames.js
@@ -1,32 +1,63 @@
 import React, {Component} from 'react' 
+import axios from 'axios';
 
 class RecentGames extends Component {
   constructor(props) {
     super(props)
-
-    this.state = {
-      
+      this.state = {
+        myGames:[]
     }
   }
 
+  componentDidMount(){
+    axios.get('/api/myGames/').then((res) => {
+      this.setState({
+        myGames: res.data
+      })
+    })
+  }
+
   render () {
+  const games = this.state.myGames.map((element, index) => {
+    let white = element.user_light
+    let black = element.user_dark
+    let winner = element.winner
+    let loser = element.loser
+    let timeCtrl = element.time_ctrl
+    let numberOfMoves = element.moves.length
+    let date = element.game_date
+    return(
+      <div key={index} className="gameRowInfo">
+      <div className="col 1">
+      <p>Date: {date}</p>
+      <p>Moves: {numberOfMoves}</p>
+      <p>Time Control: {timeCtrl}</p>
+      <p>White: {white}</p>
+      <p>Black: {black}</p>
+      </div>
+
+      <div className="col 2">
+      <p>Winner</p>
+      <p>{winner}</p>
+      </div>
+
+      <div className="col 3">
+      <p>Loser</p>
+      <p>{loser}</p>
+      </div>
+      
+
+      <div className="col 4">
+      <span>View Game</span>
+      </div>
+      </div>
+    )
+  })
     return (
       <div>
         <h2>Recent Games</h2>
         <section class="past_game_list">
-          <div className="game_data">
-            <p className="data">Date</p>
-            <p className="data">Number of moves</p>
-            <p className="data">Length of game</p>
-          </div>
-          <div className="outcome winner">
-            <h4 className="win_lose">Winner</h4>
-            <h6>username</h6>
-          </div>
-          <div className="outcome loser">
-            <h5 className="win_lose">Loser</h5>
-            <h5>username</h5>
-          </div>
+        {games}
         </section>
       </div>
     )
