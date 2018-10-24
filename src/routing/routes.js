5e5f6f81aee015ebfe7576a4bd1d6d2ddeddbef9
Jordan Smithson <jordantroysmithson@gmail.com>
2018-10-24 16:31:10 -0600

    board functionin

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -11,7 +11,7 @@ export default function Nav(){
             <Switch>
                 <Route exact path='/' component={Login}/> 
                 <Route path='/profile' component={Profile}/>
-                <Route path='/gameboard/id' component={Gameboard}/>
+                <Route path='/gameboard' component={Gameboard}/>
                 {/* // <Route path='/about' component={About}/> */}
             </Switch>
         </div>
