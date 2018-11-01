2d1759e9de4d91d128995bac97cb65e9c18f1453
Jordan Smithson <jordantroysmithson@gmail.com>
2018-11-01 10:09:42 -0600

    pre pull push 11-1-18 10:09AM

--- src/landingpage/landingpage.js
+++ src/landingpage/landingpage.js
@@ -66,14 +66,9 @@ WelcomeDiv =
     return(
         <div className="landing-page">
             <div id="title">
+                <div className="logo-img logo-right"></div>
                 <h1>Checked</h1>
-                <h3>
-                A group of chess enthusiasts checked into a hotel and were standing in the lobby
-                discussing their recent tournament victories. After about an hour, the manager 
-                came out of the office and asked them to disperse. "But why?" they asked, as they 
-                moved off. "Because," he said "I can't stand chess nuts boasting in an open foyer." 
-                <p>~ Peter Kay </p>
-                </h3>
+                <div className="logo-img logo-left"></div>
             </div>
 
             <div className="login">
