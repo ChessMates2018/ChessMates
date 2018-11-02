005af6cae86874c4a73163bdade7a48cd3634491
FreedomChaser <horsecrazyshellie@gmail.com>
2018-11-02 09:26:00 -0600

    working on jest

--- src/utils/logic.test.js
+++ src/utils/logic.test.js
@@ -1,9 +1,15 @@
 const {randomizePlayerStart} = require('./logic')
 
 describe('randomizes which player plays light or dark', () => {
-    test('challenged is truthy', () => {})
-    test('challenger is truthy', () => {})
-    test('randoCalrizian is correctly assigned a value between 0 and 1', {})
+    test('challenged is truthy', (challenged) => {
+        expect(challenged).toBeTruthy()
+    })
+    test('challenger is truthy', (chellenger) => {
+        expect(challenger).toBeTruthy()
+    })
+    test('randoCalrizian is correctly assigned a value between 0 and 1', (randoCalrizian) => {
+        
+    })
     test('user one is assigned as dark if value is less than .6', () => {})
     test('user one is assigned as light if value is greater than .6', () => {})
     test('the second user passed in gets assigned dark if user one is light', () => {})
