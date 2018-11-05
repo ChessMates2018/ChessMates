d324d71ce1948ef2d19fde4cf5df1bfde95c3f9a
Brady Snuggs <dronebug360@gmail.com>
2018-11-04 15:28:51 -1000

    finished functionality for basic feature in gameboard.  Needs identifiers (username or color) and needs scroll to always be at bottom for new messages.

--- src/gameboard/components/chat.js
+++ src/gameboard/components/chat.js
@@ -4,15 +4,60 @@ import io from 'socket.io-client'
 export default class Chat extends Component{
     constructor(){
         super()
-
+        this.state={
+            generalUserInput: "", 
+            generalMessages: [],
+            white: "",
+            black: ""
+        }
         this.socket = io.connect('/')
     }
+
+componentDidMount(){
+    this.socket = io();
+    this.socket.on("general-message", data => {
+      console.log('got general mesage');
+      let gm = this.state.generalMessages.slice();
+      gm.push(data);
+      this.setState({ generalMessages: gm });
+    });
+}
+
+submitMessage = (message, roomName) => {
+    console.log(roomName)
+    this.socket.emit(`${roomName}-chat`, message);
+}
+
+handleKeyUp = e => {
+    if (e.key === "Enter") {
+      this.submitMessage(this.state[e.target.name+'UserInput'], e.target.name);
+      this.setState({ [e.target.name+'UserInput']: "" });
+    }
+  };
+
+
     render(){
-        // var socket = io()
         return(
-            <div  className="chat">
-                <h1>I iz the chat box. LOVE ME!!!</h1>
+            <section className="chat-container">
+            <div className="messages-window">
+            {
+                this.state.generalMessages.map((gm,i)=>{
+                  return (<p key={i}>{gm}</p>)
+                })
+              }
             </div>
+            <div className="input-container">
+            <input
+            className="input-field"
+                type="text"
+                value={this.state.generalUserInput}
+                onChange={e => this.setState({ generalUserInput: e.target.value })}
+                name="general"
+                onKeyUp={this.handleKeyUp}
+            />
+            </div>
+            
+            </section>
         )
     }
 }
