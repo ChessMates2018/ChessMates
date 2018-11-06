6eab91ea1dbe0a410c9e70348cc459a432a1fa49
Brady Snuggs <dronebug360@gmail.com>
2018-11-06 10:22:17 -1000

    Working on testing of leaderboard component.

--- src/utils/Brady_logic.js
+++ src/utils/Brady_logic.js
@@ -1,20 +1,13 @@
 import axios from "axios"
 
-const leaderboardInfo = [];
 
-function getLeaderboardInfo () {
-axios.get('/api/leaderboard').then((res) => {
-return res.data
-})}
-
-getLeaderboardInfo().then(
-leaderboardInfo.push(res.data)
-)
 
 export function TopofLeaderboard(user1, user2){
-//variables
 let result;
-
+axios.get('/api/leaderboard').then((res) => {
+    console.log('TEST LOG!', res.data)
+    return res.data
+    })
 
 
 
