258f92d852a4c2502c9692acff4384e9a49c756d
Brady Snuggs <dronebug360@gmail.com>
2018-11-07 15:12:42 -1000

    Starting to style about page and setting up file structure to support it with SASS.

--- src/about/about.js
+++ src/about/about.js
@@ -3,23 +3,23 @@ import React, {Component} from 'react'
 export default class About extends Component{
     render(){
         return(
-            <div>
-                <div>
+            <section id="about-page">
+                <div className="team-mate-div">
+                    <img src='../images/posingWithPoe.jpg' alt='Brady posing with Edgar Allen Poe cutout'/>
+                    <h2>Brady Snuggs</h2>
+                    <p>A full stack web developer, chess enthusiast, FAA certified drone pilot, and musician, who enjoys working collaboratively to tackle complex problems. </p>
+                </div>
+                <div className="team-mate-div">
                     <img src='' alt=''/>
                     <h2>Name</h2>
                     <p>Bio</p>
                 </div>
-                <div>
+                <div className="team-mate-div">
                     <img src='' alt=''/>
                     <h2>Name</h2>
                     <p>Bio</p>
                 </div>
-                <div>
-                    <img src='' alt=''/>
-                    <h2>Name</h2>
-                    <p>Bio</p>
-                </div>
-            </div>
+            </section>
         )
     }
 }
